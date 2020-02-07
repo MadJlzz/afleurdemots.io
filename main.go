a23cfd2882e33332d1ce212b28ab1773663e2483
Julien Klaer <klaer.j@sfeir.com>
2020-02-07 20:15:33 +0100

    Creating the contact view.

--- main.go
+++ main.go
@@ -6,11 +6,21 @@ import (
 	"net/http"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactTemplate.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
@@ -21,7 +31,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }

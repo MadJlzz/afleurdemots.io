15fc0de10af89edd6c7f6dd9c0ac3fe905d86032
Julien Klaer <klaer.j@sfeir.com>
2020-02-12 14:51:28 +0100

    Connecting models and controllers for users.

--- controllers/user.go
+++ controllers/user.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/madjlzz/madlens/models"
 	"github.com/madjlzz/madlens/views"
 	"net/http"
 )
@@ -10,9 +11,10 @@ import (
 // This function will panic if templates are not
 // parsed correctly, and should only be used during
 // initial setup.
-func NewUsers() *Users {
+func NewUsers(us *models.UserService) *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
+		us: us,
 	}
 }
 
@@ -29,9 +31,11 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 
 type Users struct {
 	NewView *views.View
+	us *models.UserService
 }
 
 type SignupForm struct {
+	Name string `schema:"name"`
 	Email string `schema:"email"`
 	Password string `schema:"password"`
 }
@@ -44,6 +48,14 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
+	}
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	fmt.Fprintln(w, form)
 }
--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/madjlzz/madlens/controllers"
+	"github.com/madjlzz/madlens/models"
 	"net/http"
 )
 
-func must(err error) {
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	password = "password"
+	dbname = "madlens_dev"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-}
+	defer us.Close()
 
-func main() {
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
 	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+
+	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }

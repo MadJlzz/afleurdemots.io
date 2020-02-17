70447dc7729f6d8793832e4c993cac70093baf5d
Julien Klaer <klaer.j@sfeir.com>
2020-02-18 00:12:33 +0100

    Creating a image service for managing image upload.

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -6,10 +6,8 @@ import (
 	"github.com/madjlzz/madlens/context"
 	"github.com/madjlzz/madlens/models"
 	"github.com/madjlzz/madlens/views"
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 )
 
@@ -20,13 +18,14 @@ const (
 	maxMultipartMemory = 1 << 20 // 1 megabyte
 )
 
-func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
+func NewGalleries(gs models.GalleryService, is models.ImageService, r *mux.Router) *Galleries {
 	return &Galleries{
 		New:      views.NewView("bootstrap", "galleries/new"),
 		ShowView: views.NewView("bootstrap", "galleries/show"),
 		EditView: views.NewView("bootstrap", "galleries/edit"),
 		IndexView: views.NewView("bootstrap", "galleries/index"),
 		gs:       gs,
+		is:       is,
 		r:        r,
 	}
 }
@@ -37,6 +36,7 @@ type Galleries struct {
 	EditView *views.View
 	IndexView *views.View
 	gs       models.GalleryService
+	is       models.ImageService
 	r        *mux.Router
 }
 
@@ -170,13 +170,6 @@ func (g *Galleries) ImageUpload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	galleryPath := fmt.Sprintf("images/galleries/%v/", gallery.ID)
-	err = os.MkdirAll(galleryPath, 0755)
-	if err != nil {
-		vd.SetAlert(err)
-		g.EditView.Render(w, r, vd)
-		return
-	}
 
 	files := r.MultipartForm.File["images"]
 	for _, f := range files {
@@ -186,25 +179,13 @@ func (g *Galleries) ImageUpload(w http.ResponseWriter, r *http.Request) {
 			g.EditView.Render(w, r, vd)
 			return
 		}
-
-		dst, err := os.Create(galleryPath + f.Filename)
+		err = g.is.Create(gallery.ID, f.Filename, file)
 		if err != nil {
 			vd.SetAlert(err)
 			g.EditView.Render(w, r, vd)
 			return
 		}
-
-		_, err = io.Copy(dst, file)
-		if err != nil {
-			vd.SetAlert(err)
-			g.EditView.Render(w, r, vd)
-			return
-		}
-
-		file.Close()
-		dst.Close()
 	}
-
 	fmt.Fprintln(w,"Files successfully uploaded.")
 }
 
